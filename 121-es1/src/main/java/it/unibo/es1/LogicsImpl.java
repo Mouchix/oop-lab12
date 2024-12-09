cc5d7878277182ba56da50029a80a405940d513b
Marco Mondardini - marco.mondardini2@studio.unibo.it <marco.mondardini2@studio.unibo.it>
2024-12-09 12:21:03 +0100

    Final commit

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,72 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LogicsImpl implements Logics {
 
+	private final List<MyButton> listOfButton = new ArrayList<>();
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		for(int i = 0; i < size; i++) {
+			listOfButton.add(new MyButton());
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.listOfButton.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return listOfButton.stream().map(MyButton::getValue).toList();
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return listOfButton.stream().map(MyButton::isEnabled).toList();
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		if(listOfButton.get(elem).inc() == listOfButton.size() - 1) {
+			listOfButton.get(elem).setEnabled(false);
+		}
+		return listOfButton.get(elem).getValue();
+
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return listOfButton.stream().map(MyButton::getValue).map(x-> Integer.toString(x)).
+		collect(Collectors.joining("|", "<<", ">>"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		return listOfButton.stream().map(MyButton::getValue).allMatch(x -> x.equals(listOfButton.get(0).getValue()));
+	}
+
+	private class MyButton {
+		private int value = 0;
+		private boolean isEnabled = true;
+
+		public void setEnabled(boolean isEnabled) {
+			this.isEnabled = isEnabled;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public boolean isEnabled() {
+			return isEnabled;
+		}
+
+		private int inc() {
+			return this.value++;
+		}
 	}
 }
